531c50caa9ee9256925f57171333af24824949d9
Alexander Beninski <a.beninski@gmail.com>
2014-04-22 22:10:22 +0300

    initial scru.it

---
+++ Scru.it/App_Start/AuthConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using Scru.it.Models;
+
+namespace Scru.it
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: "308850452602716",
+                appSecret: "868127b136e502f010f574eef4e1acbc");
+
+            //OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
---
+++ Scru.it/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Scru.it.MvcApplication" Language="C#" %>
